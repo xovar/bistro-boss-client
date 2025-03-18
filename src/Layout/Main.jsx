40d471b29d4bc78c3ccc3ec110276a3317b4d4cf
FARHAN X0VAR <farhanx0var@FARHANs-MacBook-Air.local>
2025-03-18 14:17:53 +0600

    footer bug fix and navbar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../Pages/Footer/Footer";
+import Footer from "../Pages/shared/Footer/Footer";
+import Navbar from "../Pages/shared/Navbar/Navbar";
 
 export default function Main() {
   return (
     <div>
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import { Carousel } from "react-responsive-carousel";
+import img1 from '../../assets/'
+
+export default function Banner() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
